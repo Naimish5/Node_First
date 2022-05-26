7c5e076ee37e66959eab0c1e8eb06068a137c7d8
Naimish5 <naimishsavaliya5@gmail.com>
2022-05-26 17:32:36 +0530

    first_node

---
+++ app.js
@@ -0,0 +1,22 @@
+// var http =  require('http');
+
+// http.createServer(function(req,res){
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end("Naimish Savaliya")
+// }).listen(8000);
+
+var a=0;
+var b=100;
+
+if(a>=1){
+    var c=a+b;
+    console.log(c);
+}
+else{
+    console.log("Error!!!!!");
+}
+
+
+
+
+
